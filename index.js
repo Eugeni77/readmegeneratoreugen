91071d68a55ed79ba6831d954cb75dd55e0b5c8a
unknown <bardhoshieugen2@gmail.com>
2024-02-20 19:55:05 +0000

    changes

--- index.js
+++ index.js
@@ -5,16 +5,46 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
-
+  {
+    type: "input",
+    name: "title",
+    message: "What is the title of your project?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Describe your project",
+  },
+  {
+    type: "checkbox",
+    name: "license",
+    choices: ["MIT", "Apache 2.0", "GPL 3.0", "BSD 3", "None"],
+  },
+  {
+    type: "input",
+    name: "Contributing",
+    message: "Who contributed to this project",
+  },
+  {
+    type: "input",
+    name: "Usage",
+    message: "How to use this project",
+  },
+  {
+    type: "input",
+    name: "Installation",
+    message: "How to install this project",
+  },
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+  // This function is currently empty, you might want to implement it
 }
 
 // function to initialize program
 function init() {
-
+  // This function is currently empty, you might want to implement it
 }
 
 // function call to initialize program
